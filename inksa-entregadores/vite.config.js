e6aee7917fdd77c0f58e5a01c825913313f58554
Diego <ddiegowweber@gmail.com.br>
2025-07-02 16:45:18 -0300

    Configura .gitignore e remove node_modules do rastreamento Git. Adiciona arquivos de projeto.

--- inksa-entregadores/vite.config.js
+++ inksa-entregadores/vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path'; // Se você usar aliases, caso contrário, pode remover
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Lembre-se, use '/'
-  resolve: { // Se você usa aliases, caso contrário, pode remover
+  resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
